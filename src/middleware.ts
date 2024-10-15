a42bdf36143af7fd9afbc04c01ad91a6feb04c25
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-10-15 15:56:53 +0200

    feat: add middleware to protect route

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get('token');
+  const publicPaths = ['/', '/2fa', '/forgot-password', '/reset-password', '/system/notconnected', '/system/unauthorized'];
+  const isStaticFile = req.nextUrl.pathname.startsWith('/_next') || req.nextUrl.pathname.includes('/static');
+  const isPublicPath = publicPaths.includes(req.nextUrl.pathname);
+
+  if (isStaticFile || isPublicPath) {
+    return NextResponse.next();
+  }
+
+  if (!token) {
+    const loginUrl = new URL('/system/notconnected', req.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/:path*'],
+};
