0fbb558da6261f5ea756d6db2c188527557ad83b
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-10-19 23:12:33 +0200

    feat: add new apollo authorization & authentification

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,8 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: process.env.NEXT_PUBLIC_API_URL,
   credentials: 'include',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
