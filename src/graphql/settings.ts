8ecabe9478745182101d71a93e11dd37b8d093f4
MrBartou <anthony.denin@outlook.com>
2024-11-22 14:23:30 +0100

    feat: :building_construction: add graphql request

---
+++ src/graphql/settings.ts
@@ -0,0 +1,45 @@
+import { gql } from '@apollo/client';
+
+export const UPDATE_ADMINISTRATION = gql`
+  mutation UpdateAdministration($input: UpdateAdministrationInput!) {
+    updateAdministration(updateAdministrationInput: $input) {
+      id
+      clubName
+    }
+  }
+`;
+
+export const GET_ADMINISTRATION = gql`
+  query GetAllAdministrations {
+    getAllAdministrations {
+      id
+      clubName
+      contactEmail
+      contactPhone
+      address
+      closureDays
+      timeSlotDuration
+      reservationStartTime
+      reservationEndTime
+      maintenanceDay
+      maintenanceDuration
+      pilotLicenses
+      membershipFee
+      flightHourRate
+      clubRules
+      allowGuestPilots
+      guestPilotFee
+      fuelManagement
+    }
+  }
+`;
+
+export const GET_SETTINGS = gql`
+  query GetAllAdministrations {
+    getAllAdministrations {
+      closureDays
+      reservationStartTime
+      reservationEndTime
+    }
+  }
+`;
