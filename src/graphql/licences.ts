0391b696dc146a851b85a6a106ab5350bf218672
MrBartou <anthony.denin@outlook.com>
2024-12-06 19:19:19 +0100

    feat: :art: Change licences graphQL request

--- src/graphql/licences.ts
+++ src/graphql/licences.ts
@@ -1,12 +1,19 @@
 import { gql } from '@apollo/client';
 
 export const CREATE_LICENSE = gql`
-  mutation CreateLicense($license_type: String!, $expiration_date: DateTime!, $issue_date: DateTime!, $certification_authority: String!, $is_valid: Boolean!) {
-    createLicense(license_type: $license_type, expiration_date: $expiration_date, issue_date: $issue_date, certification_authority: $certification_authority, is_valid: $is_valid) {
+  mutation CreateLicense($createLicenseInput: CreateLicenseInput!, $documents: [Upload!]) {
+    createLicense(createLicenseInput: $createLicenseInput, documents: $documents) {
       id
+      user {
+        id
+      }
       license_type
       issue_date
-      expiry_date
+      expiration_date
+      license_number
+      certification_authority
+      status
+      documents_url
     }
   }
 `;
