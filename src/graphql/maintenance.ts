aae5b7ae7b7cbd0e6c58b88ca97cdd47ac2a53b7
MrBartou <anthony.denin@outlook.com>
2025-03-17 15:58:55 +0100

    refactor: :alien: Update graphQL Request

--- src/graphql/maintenance.ts
+++ src/graphql/maintenance.ts
@@ -11,6 +11,7 @@ export const GET_ALL_MAINTENANCES = gql`
       maintenance_cost
       images_url
       documents_url
+      status
       aircraft {
         id
         registration_number
@@ -22,5 +23,63 @@ export const GET_ALL_MAINTENANCES = gql`
         email
       }
     }
+  }
+`;
+
+export const CREATE_MAINTENANCE = gql`
+  mutation CreateMaintenance($maintenance: MaintenanceInput!) {
+    createMaintenance(maintenance: $maintenance) {
+      id
+      start_date
+      end_date
+      maintenance_type
+      description
+      maintenance_cost
+      images_url
+      documents_url
+      status
+      aircraft {
+        id
+        registration_number
+        model
+      }
+      technician {
+        id
+        first_name
+        email
+      }
+    }
+  }
+`;
+
+export const UPDATE_MAINTENANCE = gql`
+  mutation UpdateMaintenance($updateMaintenanceInput: UpdateMaintenanceInput!) {
+    updateMaintenance(updateMaintenanceInput: $updateMaintenanceInput) {
+      id
+      start_date
+      end_date
+      maintenance_type
+      description
+      maintenance_cost
+      images_url
+      documents_url
+      status
+      aircraft {
+        id
+        registration_number
+        model
+      }
+      technician {
+        id
+        first_name
+        email
+      }
+    }
+  }
+`;
+
+export const DELETE_MAINTENANCE = gql`
+  mutation DeleteMaintenance($id: Int!) {
+    deleteMaintenance(id: $id)
   }
 `;
--- src/interfaces/maintenance.ts
+++ src/interfaces/maintenance.ts
@@ -8,6 +8,7 @@ export enum MaintenanceType {
 }
 
 export type Maintenance = {
+  status: string;
   id: number;
   start_date: Date;
   end_date: Date;
